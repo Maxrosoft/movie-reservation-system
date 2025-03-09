7c826caab0d0773fac3dd580c2aea1f6289025c9
Maxrosoft <maxfilipenko07@gmail.com>
2025-03-09 11:24:54 +0100

    feat: sequelize config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import "dotenv/config";
 import authRouter from "./routes/authRouter";
+import sequelize from "./config/sequelize";
 
 const PORT: number | string = process.env.PORT || 3000;
 
@@ -8,6 +9,14 @@ const app: Express = express();
 
 app.use("/api", authRouter);
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
-});
+(async () => {
+    try {
+        await sequelize.sync();
+        console.log("Database connected successfully :)");
+        app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Database connection failed :(");
+    }
+})();
---
+++ src/config/sequelize.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+const POSTGRES_URI: string = process.env.POSTGRES_URI as string;
+
+const sequelize = new Sequelize(POSTGRES_URI);
+
+export default sequelize;
