83be29124dfca2d378deb1aad4c08184d01c4b90
Maxrosoft <maxfilipenko07@gmail.com>
2025-03-20 13:27:56 +0100

    fix: admin auth

--- src/middlewares/authenticateAdminToken.ts
+++ src/middlewares/authenticateAdminToken.ts
@@ -3,17 +3,17 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import "dotenv/config";
 import ErrorMessageI from "../interfaces/errorMessageI";
 
-const TOKEN_SECRET: string = process.env.TOKEN_SECRET as string;
+const ADMIN_TOKEN_SECRET: string = process.env.ADMIN_TOKEN_SECRET as string;
 
 export default function authenticateAdminToken(req: Request, res: Response, next: NextFunction) {
-    const { token } = req.cookies;
-    if (token == null) {
+    const { adminToken } = req.cookies;
+    if (adminToken == null) {
         const errorMessage: ErrorMessageI = { type: "error", message: "Unauthorized", code: 401 };
         return res.status(errorMessage.code).send(errorMessage);
     }
     try {
-        const decoded: JwtPayload = jwt.verify(token, TOKEN_SECRET) as JwtPayload;
-        (req as any).adminId = decoded.adminId;
+        const decoded: JwtPayload = jwt.verify(adminToken, ADMIN_TOKEN_SECRET) as JwtPayload;
+        (req as any).userId = decoded.userId;
         next();
     } catch (error) {
         next(error);
--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -3,17 +3,17 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import "dotenv/config";
 import ErrorMessageI from "../interfaces/errorMessageI";
 
-const ADMIN_TOKEN_SECRET: string = process.env.ADMIN_TOKEN_SECRET as string;
+const TOKEN_SECRET: string = process.env.TOKEN_SECRET as string;
 
 export default function authenticateToken(req: Request, res: Response, next: NextFunction) {
-    const { adminToken } = req.cookies;
-    if (adminToken == null) {
+    const { token } = req.cookies;
+    if (token == null) {
         const errorMessage: ErrorMessageI = { type: "error", message: "Unauthorized", code: 401 };
         return res.status(errorMessage.code).send(errorMessage);
     }
     try {
-        const decoded: JwtPayload = jwt.verify(adminToken, ADMIN_TOKEN_SECRET) as JwtPayload;
-        (req as any).userId = decoded.userId;
+        const decoded: JwtPayload = jwt.verify(token, TOKEN_SECRET) as JwtPayload;
+        (req as any).adminId = decoded.adminId;
         next();
     } catch (error) {
         next(error);
